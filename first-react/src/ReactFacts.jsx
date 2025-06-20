0823d57e8770bee2131323793b5777922544a295
Aakash Kumar <kumar.aakash1401@gmail.com>
2025-06-21 01:54:41 +0530

    :white_check_mark: React Facts Exercise #1 checkin

---
+++ first-react/src/ReactFacts.jsx
@@ -0,0 +1,25 @@
+import reactLogo from "../src/assets/react.svg";
+
+function ReactFacts() {
+
+    return (
+        <>
+            <div>
+                <img className="logo-margin" src={reactLogo} width="40" alt="React Logo" />
+            </div>
+
+            <h1>Fun Facts about React</h1>
+
+            <ul>
+                <li>First released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k start on the GitHub</li>
+                <li>is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+
+        </>
+    )
+}
+
+export default ReactFacts;
